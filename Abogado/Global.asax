dd37f51937b5b49033a92608219cfef866767dc5
EduardoDMTZ2019 <48335671+EduardoDMTZ2019@users.noreply.github.com>
2020-01-05 19:59:31 -0600

    listo
    listo

--- Abogado/Global.asax
+++ Abogado/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Abogado.Global" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Abogados.Global" Language="C#" %>
--- Abogado/Properties/AssemblyInfo.cs
+++ Abogado/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Abogado")]
+[assembly: AssemblyTitle("Abogados")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Abogado")]
+[assembly: AssemblyProduct("Abogados")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("00f01256-d77d-4140-9af8-c823e0f95ee8")]
+[assembly: Guid("0836ffc6-e1de-4416-8389-f94ce8dae50d")]
 
 // Version information for an assembly consists of the following four values:
 //
